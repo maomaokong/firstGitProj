5807cd066f81bf2d0154543e3623da3782fe898e
maomaogong <maomaokong@gmail.com>
2013-11-26 13:23:30 +0800

    20131126_v00.00.02

--- com/maomaogong/PersonTest.py
+++ com/maomaogong/PersonTest.py
@@ -150,7 +150,8 @@ print
 print ("Exception vs Error")
 var1 = '1'
 try:
-    var1 = var1 + 1 #since var1 is a string, it cannot be added to the number 1
+    #since var1 is a string, it cannot be added to the number 1
+    var1 = var1 + 1 
 except:
     print (var1, " is not a number") #so we execute this
 
@@ -160,7 +161,8 @@ print
 print ("Elegant Error Handling")
 var1 = '1'
 try:
-    var2 = var1 + 1 #since var1 is a string, it cannot be added to the number 1
+    #since var1 is a string, it cannot be added to the number 1
+    var2 = var1 + 1 
 except:
     var2 = int(var1) + 1
     print var2
